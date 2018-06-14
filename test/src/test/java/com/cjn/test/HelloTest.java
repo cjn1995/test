6613e4c977116b484ec05ca6f324b1b54d8f0b0e
cjn1995 <469707022@qq.com>
2018-06-14 09:27:01 +0800

    add test

--- test/src/test/java/com/cjn/test/HelloTest.java
+++ test/src/test/java/com/cjn/test/HelloTest.java
@@ -1,5 +1,11 @@
 package com.cjn.test;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.junit.Test;
 
 
@@ -7,7 +13,27 @@ public class HelloTest {
 	
 	@Test
 	public void test() {
-		System.out.println("hello world...");
+		CloseableHttpClient client = HttpClients.createDefault();
+		HttpGet get = new HttpGet("http://localhost:8080/test/hello");
+		
+		try {
+			CloseableHttpResponse response = client.execute(get);
+			try {
+				HttpEntity entity = response.getEntity(); 
+				if (entity != null) {  
+	                System.out.println("--------------------------------------");  
+	                System.out.println("Response content: " + EntityUtils.toString(entity, "UTF-8"));  
+	                System.out.println("--------------------------------------");  
+	            }  
+			} catch (Exception e) {
+				// TODO: handle exception
+			}finally {
+				response.close();
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+				
 	}
 
 }
